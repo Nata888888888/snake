cbeb60406847f256fe122b49c88c5d9e39404bf1
Nata888888888 <rozmarynovych@gmail.com>
2017-07-19 00:52:35 +0300

    Implemented moving snake

--- js/main.js
+++ js/main.js
@@ -1,19 +1,166 @@
 var c = document.getElementById('gameCanvas');
 var ctx = c.getContext("2d");
 
-ctx.clearRect(0, 0, 600, 600);
-ctx.strokeStyle = "green";
-ctx.strokeRect(250, 250, 100, 100);
+var canvas_width = 600,
+    canvas_height = 600,
+    ceil_width = 15,
+    ceil_height = 15;
+var count_row = canvas_width / ceil_width;
+var count_col = canvas_height / ceil_width;
+var defFillColor = "black";
+var defStrokeColor = "grey";
+var defFigure = "rect";
 
-ctx.beginPath();
-ctx.strokeStyle = "red";
-ctx.arc(300,300,50,0,2*Math.PI);
-ctx.stroke();
-ctx.fillStyle = "red";
-ctx.fill();
+var snakeX = Math.floor(Math.random() * (count_row - 2) + 1);
+var snakeY = Math.floor(Math.random() * (count_col - 2) + 1);
 
-ctx.moveTo(250,300);
-ctx.lineTo(350,300);
-ctx.strokeStyle = "white";
-ctx.lineWidth = 2;
-ctx.stroke();
+function clearCanvas() {
+    ctx.clearRect(0, 0, 600, 600);
+}
+
+function drawCell(x, y, figure = defFigure, fillColor = defFillColor, strokeColor = defStrokeColor) {
+    if (figure == "rect") {
+        ctx.fillStyle = fillColor;
+        ctx.fillRect(x * ceil_width + 1, y * ceil_height + 1, ceil_width - 2, ceil_height - 2);
+    } else if (figure == "circle") {
+        ctx.beginPath();
+        ctx.arc(x * ceil_width + ceil_width / 2, y * ceil_width + ceil_width / 2, ceil_height / 2 - 2, 0, 2 * Math.PI);
+        ctx.strokeStyle = strokeColor;
+        ctx.stroke();
+        ctx.fillStyle = fillColor;
+        ctx.fill();
+    }
+}
+
+function grid() {
+    for (var i = 1; i < count_col; i++) {
+        ctx.moveTo(ceil_width * i, 0);
+        ctx.lineTo(ceil_width * i, canvas_width);
+        ctx.strokeStyle = "#eaebed";
+        ctx.lineWidth = 1;
+        ctx.stroke();
+    }
+    for (var j = 1; j < count_row; j++) {
+        ctx.moveTo(0, ceil_height * j);
+        ctx.lineTo(canvas_width, ceil_height * j);
+        ctx.strokeStyle = "#eaebed";
+        ctx.lineWidth = 1;
+        ctx.stroke();
+    }
+}
+
+function buildWall() {
+    ctx.fillStyle = "#c95a06";
+    for (var i = 0; i < count_row; i++) {
+        drawCell(0, i, "rect", "#c95a06");
+    }
+    for (var i = 0; i < count_row; i++) {
+        drawCell(count_col - 1, i, "rect", "#c95a06");
+    }
+    for (var j = 0; j < count_col; j++) {
+        drawCell(j, 0, "rect", "#c95a06");
+    }
+    for (var j = 0; j < count_col; j++) {
+        drawCell(j, count_row - 1, "rect", "#c95a06");
+    }
+}
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+var apples = [];
+
+function appleFabric() {
+    var x = Math.floor(Math.random() * (count_row - 2) + 1);
+    var y = Math.floor(Math.random() * (count_col - 2) + 1);
+    var colorR = Math.floor((Math.random() * 256));
+    var colorG = Math.floor((Math.random() * 256));
+    var colorB = Math.floor((Math.random() * 256));
+
+    var apple = {
+        x: x,
+        y: y,
+        color: "rgb(" + colorR + "," + colorG + "," + colorB + ")"
+    };
+
+    return apple;
+}
+
+function dropApples() {
+    var count = randomIntFromInterval(10, 20);
+    
+    apples = [];
+    for (var i = 1; i <= count; i++) {
+        var apple = appleFabric();
+        apples.push(apple);
+    }
+}
+
+function drawApples() {
+    apples.forEach(function (apple) {
+        drawCell(apple.x, apple.y, "circle", apple.color);
+    });
+}
+
+function snake() {
+    var x = snakeX;
+    var y = snakeY;
+
+    drawCell(x, y, "circle", "green", "red");
+    // eyes
+    var xc = x * ceil_width + ceil_width / 2;
+    var yc = y * ceil_height + ceil_height / 2;
+
+    ctx.beginPath();
+    ctx.arc(xc - 3, yc - 2, 2, 0, 2 * Math.PI, false);
+    ctx.arc(xc + 3, yc - 2, 2, 0, 2 * Math.PI, false);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+
+    drawCell(x, y + 1, "circle", "green", "red");
+    drawCell(x, y + 2, "circle", "green", "red");
+}
+
+function renderFrame() {
+    clearCanvas();
+    grid();
+    buildWall();
+    drawApples();
+    snake();
+}
+
+function activateSnake(x, y) {
+    dropApples();
+    var timer = setInterval(frame, 500);
+
+    function frame() {
+        renderFrame();
+
+        if (snakeY == 1) {
+            clearInterval(timer);
+        } else {
+            snakeY--;
+        }
+    }
+}
+
+function getChar(event) {
+    if (event.keyCode = 37) {
+        return left;
+    }
+
+    if (event.keyCode = 38) {
+        return up;
+    }
+
+    if (event.keyCode = 39) {
+        return right;
+    }
+
+    if (event.keyCode = 40) {
+        return down;
+    }
+}
+
+activateSnake();
