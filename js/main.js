5d59424e04db3e2ce5c1242e1f03bfcc7f73a223
Nata888888888 <rozmarynovych@gmail.com>
2017-07-22 00:40:51 +0300

    Implemented snake movement, apples consumption, audio effects.

--- js/main.js
+++ js/main.js
@@ -10,9 +10,16 @@ var count_col = canvas_height / ceil_width;
 var defFillColor = "black";
 var defStrokeColor = "grey";
 var defFigure = "rect";
+var frameTimer;
 
 var snakeX = Math.floor(Math.random() * (count_row - 2) + 1);
 var snakeY = Math.floor(Math.random() * (count_col - 2) + 1);
+var direction = "up";
+var snakeCoords = [
+    [snakeX, snakeY],
+    [snakeX, snakeY + 1],
+    [snakeX, snakeY + 2]
+];
 
 function clearCanvas() {
     ctx.clearRect(0, 0, 600, 600);
@@ -104,24 +111,108 @@ function drawApples() {
 }
 
 function snake() {
-    var x = snakeX;
-    var y = snakeY;
-
-    drawCell(x, y, "circle", "green", "red");
+//    var x = snakeX;
+//    var y = snakeY;
+    for(var i in snakeCoords) {
+        drawCell(snakeCoords[i][0], snakeCoords[i][1], "circle", "green", "red");
+    }
     // eyes
-    var xc = x * ceil_width + ceil_width / 2;
-    var yc = y * ceil_height + ceil_height / 2;
+//    var xc = x * ceil_width + ceil_width / 2;
+//    var yc = y * ceil_height + ceil_height / 2;
+//
+//    ctx.beginPath();
+//    ctx.arc(xc - 3, yc - 2, 2, 0, 2 * Math.PI, false);
+//    ctx.arc(xc + 3, yc - 2, 2, 0, 2 * Math.PI, false);
+//    ctx.fillStyle = "yellow";
+//    ctx.fill();
 
-    ctx.beginPath();
-    ctx.arc(xc - 3, yc - 2, 2, 0, 2 * Math.PI, false);
-    ctx.arc(xc + 3, yc - 2, 2, 0, 2 * Math.PI, false);
-    ctx.fillStyle = "yellow";
-    ctx.fill();
-
-    drawCell(x, y + 1, "circle", "green", "red");
-    drawCell(x, y + 2, "circle", "green", "red");
+//    drawCell(snakeCoords[1][0], snakeCoords[1][1], "circle", "green", "red");
+//    drawCell(snakeCoords[2][0], snakeCoords[2][1], "circle", "green", "red");
 }
 
+////head return in left side
+//function snakeL() {
+//    var x = snakeX;
+//    var y = snakeY;
+//
+//    drawCell(x, y, "circle", "green", "red");
+//    // eyes
+//    var xc = x * ceil_width + ceil_width / 2;
+//    var yc = y * ceil_height + ceil_height / 2;
+////head return in left side
+//    ctx.beginPath();
+//    ctx.arc(xc - 2, yc - 3, 2, 0, 2 * Math.PI, false);
+//    ctx.arc(xc - 2, yc + 3, 2, 0, 2 * Math.PI, false);
+//    ctx.fillStyle = "yellow";
+//    ctx.fill();
+//
+//    drawCell(x + 1, y , "circle", "green", "red");
+//    drawCell(x + 2, y , "circle", "green", "red");
+//}
+//
+////head return in right side
+//
+//function snakeR() {
+//    var x = snakeX;
+//    var y = snakeY;
+//
+//    drawCell(x, y, "circle", "green", "red");
+//    // eyes
+//    var xc = x * ceil_width + ceil_width / 2;
+//    var yc = y * ceil_height + ceil_height / 2;
+//
+//    ctx.beginPath();
+//    ctx.arc(xc + 2, yc - 3, 2, 0, 2 * Math.PI, false);
+//    ctx.arc(xc + 2, yc + 3, 2, 0, 2 * Math.PI, false);
+//    ctx.fillStyle = "yellow";
+//    ctx.fill();
+//
+//    drawCell(x - 1, y , "circle", "green", "red");
+//    drawCell(x - 2, y , "circle", "green", "red");
+//}
+//
+////return down
+//function snakeD() {
+//    var x = snakeX;
+//    var y = snakeY;
+//
+//    drawCell(x, y, "circle", "green", "red");
+//    // eyes
+//    var xc = x * ceil_width + ceil_width / 2;
+//    var yc = y * ceil_height + ceil_height / 2;
+////head return down
+//    ctx.beginPath();
+//    ctx.arc(xc - 3, yc + 2, 2, 0, 2 * Math.PI, false);
+//    ctx.arc(xc + 3, yc + 2, 2, 0, 2 * Math.PI, false);
+//    ctx.fillStyle = "yellow";
+//    ctx.fill();
+//
+//    drawCell(x , y - 1 , "circle", "green", "red");
+//    drawCell(x , y - 2 , "circle", "green", "red");
+//}
+
+function eateApple(){
+    var appleCount = apples.length;
+    
+    for (var i=0; i < appleCount; i++ ){
+            if(snakeCoords[0][0] == apples[i].x && snakeCoords[0][1] == apples[i].y){
+                apples.splice(i,1);
+                var tail = snakeCoords[snakeCoords.length-1];
+                var newTail=[tail[0], tail[1]];
+                
+                snakeCoords.push(newTail);
+                
+                var audio = new Audio('est_yabloko.mp3');
+                audio.play();
+                var pause = setTimeout(function () {
+                    audio.pause();
+                    delete audio;
+                    audio = null;
+                }, 2000);
+                break;
+            }
+    }  
+}
 function renderFrame() {
     clearCanvas();
     grid();
@@ -130,37 +221,77 @@ function renderFrame() {
     snake();
 }
 
-function activateSnake(x, y) {
-    dropApples();
-    var timer = setInterval(frame, 500);
-
-    function frame() {
-        renderFrame();
-
-        if (snakeY == 1) {
-            clearInterval(timer);
-        } else {
-            snakeY--;
-        }
+function isGameOver() {
+    if (snakeY == 0) {
+        return true;
     }
+    
+    return false;
 }
 
+function moveSnake() {
+    var n = snakeCoords.length-1;
+    snakeCoords[n][0] = snakeCoords[0][0];
+    snakeCoords[n][1] = snakeCoords[0][1];
+    
+    switch(direction) {
+        case 'up':
+            snakeCoords[n][1]--;
+        break;
+        case 'left': 
+            snakeCoords[n][0]--;
+        break;
+        case 'down':  
+            snakeCoords[n][1]++;
+        break;
+        case 'right':  
+            snakeCoords[n][0]++;
+        break;
+    }
+    
+    var last = snakeCoords.pop();
+    snakeCoords.unshift(last);
+}
+
+function handlePhysics() {
+    moveSnake();
+    eateApple();
+}
+
+function frame() {
+    renderFrame();
+    handlePhysics();
+    if (isGameOver()) {
+        clearInterval(frameTimer);
+    }
+} 
+
+function activateSnake(x, y) {
+    dropApples();
+    frameTimer = setInterval(frame, 500);
+}
+
+window.addEventListener("keydown", getChar);
 function getChar(event) {
-    if (event.keyCode = 37) {
-        return left;
+    if (event.keyCode == 37) {
+        direction = "left";
     }
 
-    if (event.keyCode = 38) {
-        return up;
+    if (event.keyCode == 38) {
+        direction = "up";
     }
 
-    if (event.keyCode = 39) {
-        return right;
+    if (event.keyCode == 39) {
+        direction = "right";
     }
 
-    if (event.keyCode = 40) {
-        return down;
+    if (event.keyCode == 40) {
+        direction = "down";
     }
+    
+    frame();
+    clearInterval(frameTimer);
+    frameTimer = setInterval(frame, 500);
 }
 
 activateSnake();
